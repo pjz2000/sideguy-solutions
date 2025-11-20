aad6582966116fbc875fbf5f5a85a85a3d69f464
pjz2000 <peter.z@kromeon.com>
2025-11-20 09:43:11 -0800

    Create page.jsx

---
+++ app/page.jsx
@@ -0,0 +1,15 @@
+export default function Home() {
+
+  return (
+
+    <div style={{ padding: "40px", color: "white" }}>
+
+      <h1>SideGuy Solutions</h1>
+
+      <p>Welcome to the homepage.</p>
+
+    </div>
+
+  );
+
+}
