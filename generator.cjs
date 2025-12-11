6cda39b65a61daf4d24bc24260c40328db92131a
pjz2000 <peter.z@kromeon.com>
2025-12-11 19:00:10 +0000

    const fs = require("fs");
    // 1. Load the universal template
    
    const template = fs.readFileSync("./template.html", "utf8");
    
    // 2. Load manifest
    
    const manifest = JSON.parse(fs.readFileSync("./manifest.json", "utf8"));
    
    // 3. Generate pages
    
    manifest.forEach((page) => {
    
      const output = template
    
        .replace(/{{PAGE_TITLE}}/g, page.title)
    
        .replace(/{{META_DESCRIPTION}}/g, page.description)
    
        .replace(/{{SUBHEAD}}/g, page.subhead || "")
    
        .replace(/{{PAGE_CONTENT}}/g, page.content || "");
    
      const filename = `${page.filename}.html`;
    
      fs.writeFileSync(filename, output);
    
      console.log("Generated:", filename);
    
    });
    
    console.log("\n✔ Auto-builder finished.\n");

--- generator.cjs
+++ generator.cjs
@@ -1,22 +1,44 @@
-import fs from "fs";
-import path from "path";
+const fs = require("fs");
 
-// Load the universal template
+
+
+// 1. Load the universal template
+
 const template = fs.readFileSync("./template.html", "utf8");
 
-// Load manifest
+
+
+// 2. Load manifest
+
 const manifest = JSON.parse(fs.readFileSync("./manifest.json", "utf8"));
 
+
+
+// 3. Generate pages
+
 manifest.forEach((page) => {
+
   const output = template
+
     .replace(/{{PAGE_TITLE}}/g, page.title)
+
     .replace(/{{META_DESCRIPTION}}/g, page.description)
+
     .replace(/{{SUBHEAD}}/g, page.subhead || "")
-    .replace(/{{PAGE_CONTENT}}/g, page.content);
+
+    .replace(/{{PAGE_CONTENT}}/g, page.content || "");
+
+
 
   const filename = `${page.filename}.html`;
+
   fs.writeFileSync(filename, output);
+
   console.log("Generated:", filename);
+
 });
 
+
+
 console.log("\n✔ Auto-builder finished.\n");
+
